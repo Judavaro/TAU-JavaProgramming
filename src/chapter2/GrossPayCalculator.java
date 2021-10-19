8752bee74566df3c9fc1dd154e44aef5f54b8d27
johanht <johan@kometsales.com>
2021-10-19 17:39:00 -0500

    Primer commit

---
+++ src/chapter2/GrossPayCalculator.java
@@ -0,0 +1,13 @@
+package chapter2;
+
+public class GrossPayCalculator {
+
+    public static void main (String arg[]){
+
+      //1. Get the number of hours worked
+      System.out.println("Enter the number of hours the employee worked");
+
+
+
+    }
+}
